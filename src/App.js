4ce71f43349b94c1270db6d6ccd3142583ce14a3
naziyathedev <76842801+naziyathedev@users.noreply.github.com>
2021-03-05 02:28:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import {Container} from "@material-ui/core";
+import Form from "./Form";
+ function App() {
+  return (
+    <Container>
+      <Form/>
+      </Container>
+  
+  );
+}
+export default App;
